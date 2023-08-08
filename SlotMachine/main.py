47acef16e88d5162311e06a17fec0bec2c8fc420
BrandonBane17 <brandon.bane17@gmail.com>
2023-08-07 22:03:14 -0400

    get_number_of_lines() and get_bet() added

--- SlotMachine/main.py
+++ SlotMachine/main.py
@@ -11,4 +11,29 @@ def deposit():
             print("Please enter a number.")
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input("How many lines would you like to play? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if lines > 0 and lines <= 5:
+                break
+            else:
+                print("Number of lines must be between 1 and 5.")
+        else:
+            print("Please enter a number.")
+    return lines
+
+def get_bet():
+    while True:
+        bet = input("How much would you like to bet? $")
+        if bet.isdigit():
+            bet = int(bet)
+            if bet > 0:
+                break
+            else:
+                print("Bet must be greater than 0.")
+        else:
+            print("Please enter a number.")
+    return bet
+
